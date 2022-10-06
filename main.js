84c0a61c6b7f5ca470a7960ae709c789d98a3c2c
Santosh <sbiswa338@gmail.com>
2022-10-06 15:33:50 -0500

    Lesson 2 - My First Application

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+// Single Line Comment
+
+/** Muti-Line
+ * Comments
+ */
+
+let variableName = 'Hello';
+
+console.log(variableName);
+
+variableName = 10;
+
+console.log(variableName);
