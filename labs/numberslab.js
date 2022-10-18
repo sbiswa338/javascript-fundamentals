1851532272f447ee592ab44789e35c6261f05872
Santosh <sbiswa338@gmail.com>
2022-10-17 21:42:19 -0500

    Numbers lab for class

---
+++ labs/numberslab.js
@@ -0,0 +1,25 @@
+//Task 1 Using Numeric Separators
+
+let a = 34_038;
+let b = 45_838.8734;
+let c = 0b01_10_00_10_10_11;
+let d = 0x1_74_C0;
+let e = -895_612;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+//Task 2 console.logging 29 
+
+let literalRef = 29;
+let binary = 0b111_01;
+let Hexadecimal = 0x1D;
+let Octal = 0o35;
+
+console.log(literalRef);
+console.log(binary);
+console.log(Hexadecimal);
+console.log(Octal);
