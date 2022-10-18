fd3e63ca71a900424caef0798f8a8460db132e85
Santosh <sbiswa338@gmail.com>
2022-10-17 21:12:58 -0500

    strings lab for class

---
+++ labs/stringslab.js
@@ -0,0 +1,12 @@
+// Lab about strings
+
+// Task 1
+let labString = ' This statement has empty spaces ';
+
+console.log(labString.includes('emp'));
+console.log(labString.at(14));
+console.log(labString.trim());
+console.log(labString.toLowerCase());
+
+// Task 2 
+console.log('Jack and Jill went up the hill to fetch a pail of water.\nJack fell down and broke his crown and jill came tumbling after.')
