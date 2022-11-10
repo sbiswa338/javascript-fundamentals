05316d165686d37454313d877aa81cecf4c60cb8
sbiswa338 <112734448+sbiswa338@users.noreply.github.com>
2022-11-09 23:58:25 -0600

    Lesson 12 Concurrency Lab

---
+++ labs/asynclab.js
@@ -0,0 +1,26 @@
+/* Lesson 12 Concurrency */
+
+// Task 1
+function arugmentFunction(runs) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`This task was ran ${runs} times`);
+        }, 2000);
+    });
+}
+
+// Task 2
+async function myAsync(echo) {
+    console.log('Lets start the task');
+    const numOfRuns = await arugmentFunction(echo);
+    console.log(numOfRuns);
+}
+
+// Task 3
+function repeatFunc(){
+    for(let index = 0; index < 5; index++) {
+      myAsync(index);
+    }
+}
+
+repeatFunc();
